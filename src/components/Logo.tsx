896d04e4a24f3cddbf7f4bc6f9fd0ba6f5fcab5c
Brendan Grainger <brendan.grainger@gmail.com>
2021-11-25 18:48:59 +1300

    extracted logo and make it link back to root

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import { CodeIcon } from '@heroicons/react/outline';
 
 export default function Logo() {
   return (
-    <p className="flex items-center space-x-1 text-blue-700">
-      <CodeIcon className="w-8 h-8 flex-shrink-0" />
-      <span className="font-bold text-lg tracking-tight whitespace-nowrap">
-        Company Name
-      </span>
-    </p>
+    <Link href="/">
+      <a className="flex items-center space-x-1 text-blue-700">
+        <CodeIcon className="w-8 h-8 flex-shrink-0" />
+        <span className="font-bold text-lg tracking-tight whitespace-nowrap">
+          Company Name
+        </span>
+      </a>
+    </Link>
   )
 }
