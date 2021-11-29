7e6e48f9b23151a0cbc580851f4d3be309fd012b
Brendan Grainger <brendan.grainger@gmail.com>
2021-11-29 15:58:49 +1300

    added next-themes support

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { MoonIcon, SunIcon } from "@heroicons/react/outline";
 import Logo from "@/components/Logo";
 
 export default function Header() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  const renderThemeChanger = () => {
+    if (!mounted) return null;
+
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    if (currentTheme === 'dark') {
+      return (
+        <SunIcon className="w-7 h-7" role="button" onClick={() => setTheme('light')} />
+      )
+    } else {
+      return (
+        <MoonIcon className="w-7 h-7" role="button" onClick={() => setTheme('dark')} />
+      )
+    }
+  }
+
   return (
-    <header className="border-b border-gray-100">
-      <div className="container mx-auto px-4 sm:px-6 py-4">
+    <header className="border-b border-gray-100 dark:border-gray-700">
+      <div className="container mx-auto px-4 sm:px-6 py-4 flex justify-between items-center">
         <Logo />
+        {renderThemeChanger()}
       </div>
     </header>
   )
